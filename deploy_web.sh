52743025a1370016399ba2462500d75b95815728
dcosta500 <diogo.a.n.costa500@gmail.com>
2024-11-25 18:55:17 +0000

    Web Deployment Script done

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-remote_root="/home/diogo/documents/projects/duo_words/web_app"
-image_name="duo-words-nginx"
-container_name="$image_name-container"
+WEB_BUILD_DIR="./build/web"
+DEST_DIR="./docs"
 
 # Build web version
 flutter clean
 flutter build web
-
-# Transfer static content (html, js, css, ...)
-ssh diogo@pihome.local "rm -r $remote_root/web"
-scp -r ./build/web diogo@pihome.local:$remote_root
-
-# Rebuild image from dockerfile
-ssh diogo@pihome.local "
-cd $remote_root &&\
-docker rm -f $container_name &&\
-docker rmi duo-words-nginx
-"
-
-ssh diogo@pihome.local "
-cd $remote_root &&\
-docker build -t $image_name . &&\
-docker run --name $container_name -p 8081:80 -d $image_name
-"
+rm -r "$DEST_DIR"
+mkdir "$DEST_DIR"
+mv "$WEB_BUILD_DIR"/* "$DEST_DIR"
